ad3e452b3c769fdd2263a4b99aaaac12d202985d
Gokul Krishna <73980745+Gokulkrishna9@users.noreply.github.com>
2023-11-20 19:57:18 +0530

    cargame.py

---
+++ cargame.py
@@ -0,0 +1,22 @@
+#car game
+
+help = "Start - To start the car \n Stop - To stop the car \n Quit - To quit the game"
+a = "The car has started, Now you're ready to go..."
+b = "You've stopped the car :("
+c = "You've ended the game" 
+i = 0
+
+while i<=20:
+    Guess = str(input(">"))
+    i = i+1
+    
+    if Guess == "start":
+        print(a)
+    elif Guess == "stop":
+        print(b)
+    elif Guess == "quit":
+        break
+    elif Guess == "help":
+        print(help)
+    else:
+        print("I don't understand that ! :-(")
